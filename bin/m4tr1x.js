cd83423f6b64efe3e4c2dbb0813bf7bf077d5d2e
Felice Geracitano <fgeracitano@Felice-Geracitano-HBC-Laptop.local>
2019-01-14 16:05:18 +0000

    bigger matrix

--- bin/m4tr1x.js
+++ bin/m4tr1x.js
@@ -1,18 +1,13 @@
 #!/usr/bin/env node
 setInterval(
   _ => {
-    for (Y = y, x = 80; (x -= 2); ) {
-      Y = (Y + x * x + 9) % 27;
-      p(
-        x,
-        Y,
-        (X = "\u001B[") +
-          `37m${(C = String.fromCharCode(12450 + ((y * x) % 16)))}`
-      );
+    for (Y = y, x = 300; (x -= 2); ) {
+      Y = (Y + x * x + 9) % 80;
+      p( x, Y, (X = "\u001B[") + `37m${(C = String.fromCharCode(12450 + ((y * x) % 16)))}`);
       for (o = 6; --o; ) p(x, Y - o, X + `38;5;${46 - o * 6}m${C}`);
     }
     y++;
   },
-  (y = 99),
-  (p = (x, y, c) => y < 22 && console.log(X + `40m${X}${y};${x}H${c}`))
+  (y = 50),
+  (p = (x, y, c) => y < 50 && console.log(X + `40m${X}${y};${x}H${c}`))
 );
